1e842425ff033fb9fed0dc050be9063109c0666a
Alexander Korovka QA67 <eedc2011ru@gmail.com>
2023-07-21 18:07:19 +0300

    -a

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 20;
+        int price = 4567;
+
+        int bonus = price / amount;
+        System.out.println( "Мили: " + bonus);
+    }
+}
